b742cb37c4f7b4440290e9db38594b9651d7c3c6
locdo98 <44898288+locdo98@users.noreply.github.com>
2019-08-06 15:17:55 +0700

    commit first

---
+++ A_A_Shop_LH2_chinhthuc/src/main/java/com/dovanloc/springboot/AAShopLh2ChinhthucApplication.java
@@ -0,0 +1,13 @@
+package com.dovanloc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AAShopLh2ChinhthucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AAShopLh2ChinhthucApplication.class, args);
+	}
+
+}
